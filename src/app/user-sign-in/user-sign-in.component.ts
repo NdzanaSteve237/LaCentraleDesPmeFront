f55f825c2c2c4a7dc898a99c33532ac97a049ada
Erwin-Bidz <erwin.bidzana@institutsaintjean.org>
2024-08-11 19:26:56 +0200

    before_maps

--- src/app/user-sign-in/user-sign-in.component.ts
+++ src/app/user-sign-in/user-sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -16,20 +17,53 @@ export class UserSignInComponent implements OnInit {
     confirmPassword: ''
   };
 
-  constructor(private http: HttpClient) { }
+  registrationForm: FormGroup;
+  submitted = false;
+  showPasswords = false;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.registrationForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+   }
 
   ngOnInit(): void {
   }
 
 
+  // onSubmit() {
+  //   if (this.formModel.password !== this.formModel.confirmPassword) {
+  //     alert('Les mots de passe ne correspondent pas');
+  //     return;
+  //   }
+
+  //   // Envoyer les données du formulaire via une requête HTTP POST
+  //   this.http.post('https://votre-api.com/endpoint', this.formModel)
+  //     .subscribe(response => {
+  //       console.log('Réponse du serveur', response);
+  //     }, error => {
+  //       console.error('Erreur lors de l\'envoi des données', error);
+  //     });
+  // }
+
   onSubmit() {
-    if (this.formModel.password !== this.formModel.confirmPassword) {
+    this.submitted = true;
+
+    // S'arrêter si le formulaire est valide
+    if (this.registrationForm.invalid) {
+      return;
+    }
+
+    if (this.registrationForm.value.password !== this.registrationForm.value.confirmPassword) {
       alert('Les mots de passe ne correspondent pas');
       return;
     }
 
     // Envoyer les données du formulaire via une requête HTTP POST
-    this.http.post('https://votre-api.com/endpoint', this.formModel)
+    this.http.post('https://votre-api.com/endpoint', this.registrationForm.value)
       .subscribe(response => {
         console.log('Réponse du serveur', response);
       }, error => {
@@ -37,5 +71,12 @@ export class UserSignInComponent implements OnInit {
       });
   }
 
+  toggleShowPasswords() {
+    this.showPasswords = !this.showPasswords;
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    const confirmPasswordInput = document.getElementById('confirm-password') as HTMLInputElement;
+    passwordInput.type = this.showPasswords ? 'text' : 'password';
+    confirmPasswordInput.type = this.showPasswords ? 'text' : 'password';
+  }
 
 }
